63604f72dc3e382821b8455cf649d5a4a06a2f84
Daniel Popescu <danielpopescu95@gmail.com>
2012-02-08 00:14:35 +0200

    adding RXTX library. Now you can read serial data from Arduino. No dependencies in Maven pom yet.

--- arduino/src/main/java/ro/danielp/arduino/webclient/controller/FrontController.java
+++ arduino/src/main/java/ro/danielp/arduino/webclient/controller/FrontController.java
@@ -3,14 +3,19 @@ package ro.danielp.arduino.webclient.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import ro.danielp.arduino.core.SerialTest;
 
 @Controller
 public class FrontController {
 
     @RequestMapping(value = {"*", "/", "dashboard", "home"})
-    public ModelAndView showDashboard() {
+    public ModelAndView showDashboard() throws Exception {
+        SerialTest.main();
+
         ModelAndView modelAndView = new ModelAndView("dashboard");
         return modelAndView;
     }
 
 }
+
+
